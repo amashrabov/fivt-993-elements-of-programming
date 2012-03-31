c5c9a95b6f57beb95599dea4f82142fdcb822c74
AlexTaran <alex.y.taran@gmail.com>
2012-03-31 18:16:21 +0400

    Fenwick tree beta version

--- stlext/fenwick_tree.h
+++ stlext/fenwick_tree.h
@@ -1,14 +1,15 @@
-#ifndef _FENWICK_TREE_H_
-#define _FENWICK_TREE_H_
+// Copyright 2012 Alex Taran
+#ifndef STLEXT_FENWICK_TREE_H_
+#define STLEXT_FENWICK_TREE_H_
 
-#include <algorithm>
-#include <vector>
 #include <array>
-
-#include <cassert>
-#include <cstdarg>
+#include <functional>
+#include <vector>
 
 using std::vector;
+using std::array;
+using std::cout;
+using std::endl;
 
 /*
 Author: Alex Taran
@@ -28,72 +29,104 @@ template<size_t N, class T,
   class SubFunc = std::minus<T> >
   class FenwickTree {
    public:
-    typedef vector<size_t> Coord;
+    typedef array<size_t, N> Coord;
 
-    FenwickTree<N, T, AddFunc, SubFunc> () : dims_(N, 0){
+    FenwickTree<N, T, AddFunc, SubFunc> () {
+      dims_.fill(0);
       reset();
     }
 
     FenwickTree<N, T, AddFunc, SubFunc> (const Coord& dims) : dims_(dims) {
-      assert(dims.size() == N);
       reset();
-    }
-    /*
-    T& operator [] (const Coord& c) {
-      return getElement(c);
     }
 
     const T& operator[] (const Coord& c) const {
-      return getElement(c);
+      return get(c);
     }
-    */
+
     Coord size() const {
       return dims_;
     }
 
     T aggregate(const Coord& c) const {
-      assert(c1.size() == N);
       Coord curr = c;
-      while () { // some condition here
-        //TODO: iterate for 'curr'
-        //TODO: aggregate a result
-      }
-    }
-
-    T aggregate(const Coord& c1,const Coord& c2) const {
-      assert(c1.size() == N);
-      assert(c2.size() == N);
-      for(size_t i = 0; i < N; ++i) {
-        assert(c1[i]<=c2[i]);
-      }
-      // I/E formula here
-      Coord c = c2;
-      T result = aggregate(c);
-      for(size_t i = 1; i < (1<<N); ++i) { // bitmask
-        for(size_t j = 0; j < N; ++j) { // replace bits
-          if( (i>>j)&1 == 1){
-            c[j] = c1[j];
+      T result = getPrevElement(c);
+      while (true) {
+        // iterate for 'curr'
+        size_t i;
+        for (i = N - 1; i > 0; --i) {
+          if ((curr[i] & (curr[i] + 1))  <= 1) {
+            curr[i] = c[i];
+          } else {
+            break;
           }
         }
-        // here count NBITS and use AddFunc if even, SubFunc - if odd
+        curr[i] = (curr[i] & (curr[i] + 1));
+        if (curr[0] == 0) {
+          break;
+        }
+        curr[i]--;
+        // aggregate a result
+        result = addFunc_(result, getPrevElement(curr));
       }
       return result;
     }
 
-    void set(const Coord& c,const T& value){
-      assert(c.size() == N);
-      Coord curr = c;
-      // TODO: Some street magic which does the job
+    T aggregate(const Coord& c1, const Coord& c2) const {
+      for (size_t i = 0; i < N; ++i) {
+        assert(c1[i] <= c2[i]);
+      }
+      // I/E formula here
+      Coord c = c2;
+      T result = aggregate(c);
+      bool negation = false;
+      for (size_t i = 1; i < (1<<N); ++i) {  // bitmask
+        size_t gray = i^(i>>1);
+        negation = !negation;
+        for (size_t j = 0; j < N; ++j) {  // replace bits
+          if ((gray >> j) & 1 == 1) {
+            c[j] = c1[j];
+          } else {
+            c[j] = c2[j];
+          }
+        }
+        if (negation) {
+          result = subFunc_(result, aggregate(c));
+        } else {
+          result = addFunc_(result, aggregate(c));
+        }
+      }
+      return result;
     }
 
-    T get(const Coord& c){
-      asserd(c.size() == N);
+    void set(const Coord& c, const T& value) {
+      Coord curr = c;
+      T delta = subFunc_(value, get(c));
+      do {
+        incElement(curr, delta);
+        size_t i;
+        for (i = N - 1; i > 0; --i) {
+          if (curr[i]|(curr[i]+1) >= dims_[i]) {
+            curr[i] = c[i];
+          } else {
+            break;
+          }
+        }
+        curr[i] = curr[i] | (curr[i] + 1);
+        if (curr[0] >= dims_[0]) {
+          break;
+        }
+      } while (true);
+    }
+
+    T get(const Coord& c) const {
       Coord c1 = c;
       for (size_t i = 0; i < N; ++i) {
         c1[i]++;
       }
-      return aggregate(c,c1);
+      return aggregate(c, c1);
     }
+
    private:
     Coord dims_;
     Coord koefs_;
@@ -103,7 +136,7 @@ template<size_t N, class T,
     SubFunc subFunc_;
     T neutralElem_;
 
-    static size_t power (size_t a, size_t p) {
+    static size_t power(size_t a, size_t p) {
       size_t res = 1;
       for (size_t i = 0; i < p; ++i) {
         res *= a;
@@ -111,7 +144,7 @@ template<size_t N, class T,
       return res;
     }
 
-    void reset () { /* resets koefs_ and data_ size*/
+    void reset() { /* resets koefs_ and data_ size*/
       size_t k = 1, i = N;
       do {
         --i;
@@ -119,10 +152,10 @@ template<size_t N, class T,
         k *= dims_[i];
       } while (i > 0);
       data_.resize(k);
+      neutralElem_ = subFunc_(data_[0], data_[0]);
     }
 
-    T& getElement(const Coord& c) { /* gets element by coordinate*/
-      assert(c.size() == N);
+    const T& getElement(const Coord& c) const { /* gets element by coordinate*/
       size_t k = 0;
       for (size_t i = 0; i < N; ++i) {
         k += koefs_[i] * c[i];
@@ -130,7 +163,37 @@ template<size_t N, class T,
       assert(k < data_.size());
       return data_[k];
     }
+
+    const T& getPrevElement(const Coord& c) const {
+      size_t k = 0;
+      for (size_t i = 0; i < N; ++i) {
+        if (c[i] == 0) {
+          return neutralElem_;
+        }
+        k += koefs_[i] * (c[i]-1);
+      }
+      assert(k < data_.size());
+      return data_[k];
+    }
+
+    void incElement(const Coord& c, const T& value) {
+      size_t k = 0;
+      for (size_t i = 0; i < N; ++i) {
+        k += koefs_[i] * c[i];
+      }
+      assert(k < data_.size());
+      data_[k] = addFunc_(data_[k], value);
+    }
+
+    static bool isZeroCoord(const Coord& c) {
+      for (size_t i = 0; i < N; ++i) {
+        if (c[i] != 0) {
+          return false;
+        }
+      }
+      return true;
+    }
   };
 }   // namespace stlext
 
-#endif  // _FENWICK_TREE_H_
+#endif  // STLEXT_FENWICK_TREE_H_
