216251b31d521dbb3873961a640deb9f39d10f85
DPR-pavlin <dprpavlin@gmail.com>
2012-04-02 15:33:43 +0400

    compiled_version

---
+++ ololo/main.cpp
@@ -0,0 +1,24 @@
+#include "PersSegmTreeML.h"
+
+#include <iostream>
+#include <cstdio>
+#include <memory>
+
+
+int main()
+{
+
+	double x;
+	PersistantSegmentTree<double> tree(true, 10, x);
+	tree.assign(2, 6, 1.2);
+
+	printf("%.10lf\n", tree.findSum(5, 7));
+	printf("%.10lf\n", tree.findSum(1, 1));
+	printf("%.10lf\n", tree.findSum(2, 5));
+
+	tree.add(4, 9, -2);
+
+	printf("%.10lf\n", tree.findSum(2, 5));
+
+	return 0;
+}
