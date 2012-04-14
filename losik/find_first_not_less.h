369f6e89823508d20bc45a25a43392c258d2a646
losik <losik@calculate.local>
2012-04-14 18:37:30 +0400

    Find first not less major modifications.

--- losik/find_first_not_less.h
+++ losik/find_first_not_less.h
@@ -1,6 +1,7 @@
 #ifndef _FIND_FIRST_NOT_LESS_H_
 #define _FIND_FIRST_NOT_LESS_H_
 
+#include <gtest/gtest.h>
 #include <functional>
 #include <algorithm>
 #include <iterator>
@@ -12,78 +13,80 @@
 // If no such element is found returns end.
 // Function's output is quite the same as lower_bound 
 template<class Iterator, class T, class Comparator>
-Iterator find_first_not_less(Iterator begin, Iterator end, const T& element, Comparator less) {
-  Iterator it(begin);
-  Iterator it_next(it);
-  Iterator temp;
-  
-  greater_or_equal<Comparator> not_less(less);  
-  
+Iterator find_first_not_less(Iterator begin, Iterator end, const T& element, const Comparator& less) {
+  // This variable is used to make test steps.
+  // Thou in part A and part B the steps made
+  // are different - the main idea remains.
+  Iterator it_next(begin);
+
+  // The size of the following step.
+  // Used both in A and B parts of the algorithm.
   typename std::iterator_traits<Iterator>::difference_type step = 1;
-  typename std::iterator_traits<Iterator>::difference_type avaliable_distance;
 
+  // The size of an interval we are interested in at the moment.
+  typename std::iterator_traits<Iterator>::difference_type avaliable_distance = std::distance(begin, end);
 
-  // Checking for extreme caseslf lf 
-  if (std::distance(begin, end) <= 0) return end;
-  if (not_less(*begin, element)) return begin;
+  // The only thing algorithm uses is that begin <= end.
+  // If this is wrong - exiting.
+  if (avaliable_distance <= 0)
+    return end;
+
+  // ALOGORITHM'S PART A 
   
-  avaliable_distance = std::distance(it_next, end);
-
-
-  // ALOGORITHM'S STEP 1
-  //
-  // After this one it refers to an object < than element
-  // and it_next to an object compared >= than element or
-  // if no such to the end.
-  //
-  // Also note that after this part of algorithm step > (it_next, it]
-
-  // Each iteration of the cycle doubles the distance between
-  // it & it_next till we find a proper pair of iterators (see
-  // comment above) or next iteration would make one of
-  // iterators out of range.
-  while (avaliable_distance > step) {
-    if (not_less(*it_next, element))
+  // In this part  we decrease the distance of [begin, end)
+  // so that it is still a valid search interval.
+  // Here 'avaliable_distance' represents the distance of
+  // [it_next, end) but after this cycle it will represent
+  // the distance of [begin, end).
+  while (true) {
+    // Here we make sure that 'begin' can be made 'it_next'
+    // so that [begin, end) would be still a valid search
+    // interval. If not - [begin, it_next) is the last valid.
+    if (!less(*it_next, element)) {
+      end = it_next;
+      step >>= 1;
+      avaliable_distance = step;
       break;
+    }
+ 
+    // Making step forward. Which means begin = it_next,
+    // it_next = it_next + step.
+    // 'begin' is guaranteed to be valid after this but 
+    // it_next may become an out of range Iterator.
+    begin = it_next;
 
-    it = it_next;
-    it_next += step;
+    // So here we check if 'it_next' would become fine
+    // after incrementation. If yes - there is no point
+    // in incrementing it. [begin, end) is the last valid.
+    if (avaliable_distance <= step) {
+      avaliable_distance = end - begin;
+      break;
+    }
+    
     avaliable_distance -= step;
+    it_next += step;
     step <<= 1;
   }
 
-  // One more step may be required due to step is always 
-  // a power of 2.
-  if (less(*it_next, element)) {
-    it = it_next;
-    it_next = end;
-  }
+  // Invariant check.
+  assert(avaliable_distance == std::distance(begin, end));
 
-
-  // ALOGITHM'S STEP 2
+  // ALOGITHM'S PART B
   //
-  // Searching on the (it, it_next]. After this one we get the answer.
-
-  // Now this variable maintains relevant distance between it_next and it
-  avaliable_distance = it_next - it;
-
-  // Each iteration 0 <= avaliable_distance <= 2 * step and step is twice
-  // less than on previous iteration. 
-  // Iterator it referes to an object compared less than element
+  // Searching on the [begin, end). From the end.
+  // Trying to get as close to begin as we can.
   while (step != 0) {
     if (avaliable_distance > step) {
-      temp = it + step;
-      if (less(*temp, element)) {
-        it = temp;
-        avaliable_distance -= step;
+      it_next = end - step;
+      if (!less(*it_next, element)) {
+        end = it_next;
+	avaliable_distance -= step;
       }
     }
     step >>= 1;
-  } 
-  
-  // Cause it is the last element compared less than element
-  // returning ++it
-  return ++it;
+  }
+
+  return end;
 }
 
 template <class Iterator, class T>
