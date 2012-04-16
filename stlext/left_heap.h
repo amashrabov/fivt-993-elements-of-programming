dc2f35d806e37bb78691fb63c480ee064c86a245
Dmitry Anikushin <dmitry.anikuhsin@gmail.com>
2012-04-16 20:52:14 +0400

    Добавлен "быстрый" конструктор и push от итераторов, не создающие промежуточные копии

--- stlext/left_heap.h
+++ stlext/left_heap.h
@@ -3,6 +3,7 @@
 
 #include <algorithm>
 #include <functional>
+#include <vector>
 #include <memory>
 #include <iostream>
 
@@ -15,6 +16,7 @@
 
 namespace psd{
 
+namespace internal{
 template<class T>
 struct node{
 public:
@@ -22,6 +24,10 @@ public:
   
   class const_node_ptr : public std::shared_ptr<const node<T>> {
 	public:
+		const_node_ptr operator=(node_ptr nd){
+			this->base_class::operator=(static_cast<base_class>(nd));
+			return *this;
+		}
     node_ptr clone(){
       node_ptr my_clone(new node(**this));
       this->base_class::operator=(static_cast<base_class>(my_clone));
@@ -30,23 +36,24 @@ public:
     void make_leaf(const T &value) {
       this->reset(new node(value));
     }
+
   private:
     typedef std::shared_ptr<const node<T>> base_class;
   };
 
   explicit node (const T &key)
-    : key(key),s_value(1){
+		: key(key), s_value(1) {
   }
 
   node (const T &key, const_node_ptr left, const_node_ptr right)
-    : key(key){
+    : key(key) {
     left_child=left;
     right_child=right;
     count_s_value();
   }
 
   void fix_leftlist(){
-    if(left_child == nullptr){
+    if(left_child == nullptr) {
 			std::swap(left_child,right_child);
       s_value = 1;
     }
@@ -76,54 +83,67 @@ public:
 
 template<class T, class Comparator>
 typename node<T>::const_node_ptr merge_nodes(
-			typename node<T>::const_node_ptr Left, 
-			typename node<T>::const_node_ptr Right,
-			Comparator &cmp){
-  if(Left==nullptr)
-    return Right;
-  if(Right==nullptr)
-    return Left;
-  if(cmp(Right->key, Left->key)) {  
-		std::swap(Left,Right);
+			typename node<T>::const_node_ptr left, 
+			typename node<T>::const_node_ptr right,
+			Comparator cmp){
+  if(left==nullptr)
+    return right;
+  if(right==nullptr)
+    return left;
+  if(cmp(right->key, left->key)) {  
+		std::swap(left, right);
   }
-  typename node<T>::node_ptr left = Left.clone();
+  typename node<T>::node_ptr mutable_left = left.clone();
+										// left point to new "clonned" version of node
 
-  left->right_child = merge_nodes<T, Comparator> 
-												(left->right_child, Right, cmp);
-  left->fix_leftlist();
+  mutable_left->right_child = merge_nodes<T, Comparator> 
+												(mutable_left->right_child, right, cmp);
 
-  return Left;
+  mutable_left->fix_leftlist();
+
+  return left;
 }
+
+template<class T, class Comparator>
+struct node_cmp{
+	node_cmp(Comparator cmp) : cmp_(cmp) {
+	}
+	bool operator()(const node<T> *left,const node<T> *right) {
+		return cmp_(left->key,right->key);
+	}
+private:
+	Comparator cmp_;
+};
+
+} // namespace internal 
 
 template<class T, class Comparator = std::less<T> >
 class left_heap{
 public:
-  typedef typename node<T>::node_ptr node_ptr;
-  typedef typename node<T>::const_node_ptr const_node_ptr;
+  typedef typename internal::node<T>::node_ptr node_ptr;
+  typedef typename internal::node<T>::const_node_ptr const_node_ptr;
 
   left_heap()
-    : root_(),size_(0) {
+    : root_(), size_(0) {
   }
-
-  left_heap(Comparator cmp)
-    : root_(),size_(0), cmp_(cmp) {
+		
+  explicit left_heap(Comparator cmp)
+    : root_(), size_(0), cmp_(cmp) {
   }
 
 	template<class Iterator>
   left_heap(Iterator begin, Iterator end) :
       root_(), size_(0) {
-    for (Iterator it = begin; it != end; ++it) {
-      this->push(*it);
-    }
+		clear();
+		push(begin,end);
   }
 
 
   template<class Iterator>
   left_heap(Iterator begin, Iterator end, Comparator cmp) :
-      cmp_(cmp), root_(), size_(0) {
-    for (Iterator it = begin; it != end; ++it) {
-      this->push(*it);
-    }
+		root_(), size_(0), cmp_(cmp) {
+		clear();
+		push(begin, end);
   }
 
   left_heap(const left_heap<T> &base_heap)
@@ -135,24 +155,11 @@ public:
     size_ = base_heap.size_;
 		cmp_  = base_heap.cmp_ ;
   }
-
-/*
-	~left_heap(){
-		while(size_ >= max_destructor_size_){
-			pop();
-		}	
-	}
-	*/
 	
 	~left_heap(){
-		while(root_!=nullptr){
-			root_ = root_->left_child; // We save shared_ptr to left child, and
-																// "delete" old root and right child.
-																// Heigt of right subtree less O(log N)
-		}
+		clear();
 	}
 	
-
   void push(const T &key){
     if(root_ == nullptr){
       size_ = 1;
@@ -162,38 +169,69 @@ public:
       size_++;
       const_node_ptr new_node;
       new_node.make_leaf(key);
-      root_ = merge_nodes<T, Comparator> (root_, new_node, cmp_);
+      root_ = internal::merge_nodes<T, Comparator> (root_, new_node, cmp_);
     }
   }
+
+	template<class Iterator>
+	void push(Iterator begin, Iterator end) {
+		std::vector<internal::node<T> *> new_elements;
+		for (Iterator it = begin; it != end; ++it) {
+			new_elements.push_back(new internal::node<T> (*it));
+		}
+		std::sort(new_elements.begin(),new_elements.end(),internal::node_cmp<T,Comparator>(cmp_));
+		size_t n = new_elements.size();
+		for(size_t i = n-1; i > 0; i--) {
+			new_elements[i]->fix_leftlist();
+			if( i%2 == 0 ) { //(i-1)/2 - parent of this element
+				new_elements[(i-1)/2]->right_child = new_elements[i];
+			}
+			else{
+				new_elements[(i-1)/2]->left_child = new_elements[i];
+			}
+		}
+		new_elements[0]->fix_leftlist();
+		const_node_ptr new_root;
+		new_root = new_elements[0];
+		root_ = internal::merge_nodes<T> (root_, new_root, cmp_);
+  }
+
   void pop(){
-    root_ = merge_nodes<T, Comparator> 
+    root_ = internal::merge_nodes<T, Comparator> 
 							(root_->left_child, root_->right_child, cmp_);
     size_--;
   }
+
   void merge(const left_heap &tree){
-    root_ = merge_nodes<T, Comparator> (root_, tree.root_, cmp_);
+    root_ = internal::merge_nodes<T, Comparator> (root_, tree.root_, cmp_);
     size_ += tree.size();
   }
+
   T top() const{
     return root_->key;
   }
+
   size_t size() const{
     return size_;
   }
+
+	void clear(){
+		while(root_!=nullptr){
+			root_ = root_->left_child; // We save shared_ptr to left child, and
+																// "delete" old root and right child.
+																// Height of right subtree less O(log N)
+		}
+		size_ = 0;
+	}
+
   bool empty() const{
     return size_==0;
   }
+
 private:
   const_node_ptr root_;
   size_t size_;
 	Comparator cmp_;
-	/*
-#ifdef _DEBUG
-	static const size_t max_destructor_size_ = 500; // ????
-#else 
-	static const size_t max_destructor_size_ = 10000; // ????
-#endif //ifdef DEBUG
-	*/
 };
 
 } //namespace psd
